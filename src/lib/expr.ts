4670bb495b27ce958c7f29e36020a29753882521
Neill Johnston <neilljohnston30@gmail.com>
2024-01-08 21:13:28 -0500

    Node adapter and tree shaking stupidity

--- src/lib/expr.ts
+++ src/lib/expr.ts
@@ -6,15 +6,20 @@
 
 import type { Data } from "./api";
 
-const fix = (n: number, k: number) => n.toFixed(k);
+export const fix = (n: number, k: number) => n.toFixed(k);
+export const clamp = (n: number, a: number, b: number) => Math.min(b, Math.max(a, n));
 
-const clamp = (n: number, a: number, b: number) => Math.min(b, Math.max(a, n));
+// Hacky fix to ensure that these values don't get tree shook. I believe the
+// logic here is just slightly too complex for the shaker to prove that it never
+// does anything, so the expression (and therefore the values) get left in.
+const protect = (f: any[]) => { !f && console.log(); };
+protect([fix, clamp]);
 
 export const expr = (expr: string, { time, sys, cpu, gpu, mem, os, pre }: Data) => {
     try {
         return eval(expr).toString();
     }
     catch {
-        return `<span style="color: red;">${expr}</span>`
+        return `<span class="error">${expr}</span>`
     }
-}
+};
